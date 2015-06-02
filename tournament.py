30b849a7781bad1cfb5216893249d8d6c746dfea
Cissy118 <Cissy118@users.noreply.github.com>
2015-06-01 21:26:44 -0700

    Initial

--- tournament.py
+++ tournament.py
@@ -92,37 +92,7 @@ def reportMatch(winner, loser):
     db_cursor.execute(query, (winner,loser))
     db.commit()
     db.close()
- 
- 
-def swissPairings_basic():
-    """Returns a list of pairs of players for the next round of a match.
-  
-    Assuming that there are an even number of players registered, each player
-    appears exactly once in the pairings.  Each player is paired with another
-    player with an equal or nearly-equal win record, that is, a player adjacent
-    to him or her in the standings.
-  
-    Returns:
-      A list of tuples, each of which contains (id1, name1, id2, name2)
-        id1: the first player's unique id
-        name1: the first player's name
-        id2: the second player's unique id
-        name2: the second player's name
-    """
-
-    standingsList = playerStandings()
-    i = 0
-    while i < len(standingsList):
-        id1 = standingsList[i][0]
-        name1 = standingsList[i][1]
-        id2 = standingsList[i + 1][0]
-        name2 = standingsList[i + 1][1]
-        if i == 0:
-            result = [(id1, name1, id2, name2)]
-        else:
-            result.append((id1, name1, id2, name2))
-        i = i + 2
-    return result
+    
 
 def swissPairings():
     """Returns a list of pairs of players for the next round of a match.
